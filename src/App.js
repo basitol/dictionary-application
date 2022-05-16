3e339b5399cbfee423c60d7a6a493885e18ed379
basitol <41623944+basitol@users.noreply.github.com>
2022-05-16 23:32:52 +0100

    Made Api call with axios,added styles to container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
+import { Container } from "@mui/material";
+import { createTheme } from "@mui/system";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Header from "./Components/Header/Header";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      primary: "#fff",
+    },
+  },
+});
 
 function App() {
+  const [words, setWords] = useState([]); // words is an array of objects
+  // const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+      );
+      // console.log(data);
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
   return (
-    <div className="App">
-      <div>Helllo world</div>
+    <div
+      className="App"
+      style={{ backgroundColor: "#222", height: "100vh", color: "#fff" }}
+      theme={darkTheme}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <Header />
+        Helllo world
+      </Container>
     </div>
   );
 }
