8023f84dbf549dd51f48c67cb45096f9bf4452df
basitol <41623944+basitol@users.noreply.github.com>
2022-05-21 17:21:26 +0100

    build examples, and definitions

--- src/Components/Definitions/Definitions.jsx
+++ src/Components/Definitions/Definitions.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import "./Definitions.css";
 
-const Definitions = () => {
-  return <div>Definitions</div>;
+const Definitions = ({ meanings, words, category }) => {
+  return (
+    <div className="definitions">
+      <div className="meanings">
+        {words === "" ? (
+          <span className="subTitle">Start by typing a word</span>
+        ) : (
+          meanings.map((meaning) =>
+            meaning.meanings.map((item) =>
+              item.definitions.map(
+                (def) => (
+                  console.log(def),
+                  (
+                    <div
+                      className="definition"
+                      style={{ color: "#000", backgroundColor: "#fff" }}
+                    >
+                      <b>{def.definition}</b>
+                      <hr
+                        style={{
+                          backgroundColor: "black",
+                          width: "100%",
+                          height: "1px",
+                        }}
+                      />
+                      {def.example && (
+                        <span>
+                          <b>Example : </b> {def.example}
+                        </span>
+                      )}
+                      {def.synonyms.length > 0 && (
+                        <span>
+                          <b>Synonyms : </b>
+                          {def.synonyms.map((synonym) => `${synonym},`)}
+                        </span>
+                      )}
+                    </div>
+                  )
+                )
+              )
+            )
+          )
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Definitions;
